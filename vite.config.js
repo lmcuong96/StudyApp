ae6a5f7c316ed9a19e9ab806cc845bf4bda402e2
Cuong Le <lmchaha96@gmail.com>
2026-03-27 01:39:17 +0700

    feat: add base path configuration to support GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Hỗ trợ tên miền con của GitHub Pages
+
   plugins: [
     react(),
     tailwindcss(),
